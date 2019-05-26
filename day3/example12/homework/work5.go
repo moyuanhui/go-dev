14763715be91ba8ff9879fbcdc19a08c4cef6e2b
allen <moyhui@163.com>
2019-05-26 16:01:11 +0800

    超过int64的大数相加

--- day3/example12/homework/work5.go
+++ day3/example12/homework/work5.go
@@ -13,7 +13,8 @@ func MaxSum(str string) {
 		if len(strSlice) == 2 {
 			strNumber1 := strings.TrimSpace(strSlice[0])
 			strNumber2 := strings.TrimSpace(strSlice[1])
-			calc(strNumber1, strNumber2)
+			result := calc(strNumber1, strNumber2)
+			fmt.Println(result)
 		} else {
 			fmt.Println("please input a+b")
 			return
@@ -24,6 +25,60 @@ func MaxSum(str string) {
 }
 
 func calc(str1, str2 string) (result string) {
+
+	if len(str1) == 0 && len(str2) == 0 {
+		result = "0"
+		return
+	}
+	index1 := len(str1) - 1
+	index2 := len(str2) - 1
+	var left int
+
+	for index1 >= 0 && index2 >= 0 {
+
+		c1 := str1[index1] - '0'
+		c2 := str2[index2] - '0'
+		sum := int(c1) + int(c2) + left
+
+		if left >= 10 {
+			left = 1
+		} else {
+			left = 0
+		}
+		c3 := (sum % 10) + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+		index1--
+		index2--
+	}
+
+	for index1 >= 0 {
+		c1 := str1[index1] - '0'
+		sum := int(c1) + left
+		if sum >= 10 {
+			left = 1
+		} else {
+			left = 0
+		}
+		c3 := (sum % 10) + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+		index1--
+	}
+
+	for index2 >= 0 {
+		c1 := str2[index2] - '0'
+		sum := int(c1) + left
+		if sum >= 10 {
+			left = 1
+		} else {
+			left = 0
+		}
+		c3 := (sum % 10) + '0'
+		result = fmt.Sprintf("%c%s", c3, result)
+		index2--
+	}
+	if left == 1 {
+		result = fmt.Sprintf("%c%s", left, result)
+	}
 
 	return
 }
