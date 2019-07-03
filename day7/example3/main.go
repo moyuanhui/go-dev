570290c9820f390ead529bb66e6266cee05abb5f
莫元辉 <moyhui@hengkangit.com>
2019-07-03 09:15:32 +0800

    xx

---
+++ day7/example3/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type student struct {
+	Name  string
+	Age   int
+	Score float32
+}
+
+func main() {
+	str := "stu01 18 89.98"
+	var stu student
+	fmt.Sscanf(str, "%s %d %f", &stu.Name, &stu.Age, &stu.Score)
+	fmt.Println(stu)
+}
